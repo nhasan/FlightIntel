eb7d4c189173b0e77682a7424c7cbdc0204d868a
Nadeem Hasan <nhasan@nadmm.com>
2011-04-29 23:34:26 -0400

    Remarks activity.

---
+++ src/com/nadmm/airports/AirportRemarksActivity.java
@@ -0,0 +1,148 @@
+/*
+ * Airports for Android
+ *
+ * Copyright 2011 Nadeem Hasan <nhasan@nadmm.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package com.nadmm.airports;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.TableLayout.LayoutParams;
+
+import com.nadmm.airports.DatabaseManager.Airports;
+import com.nadmm.airports.DatabaseManager.Remarks;
+import com.nadmm.airports.DatabaseManager.Runways;
+
+public class AirportRemarksActivity extends Activity {
+
+    private LinearLayout mMainLayout;
+    private LayoutInflater mInflater;
+
+    @Override
+    protected void onCreate( Bundle savedInstanceState ) {
+        super.onCreate( savedInstanceState );
+
+        requestWindowFeature( Window.FEATURE_INDETERMINATE_PROGRESS );
+
+        mInflater = getLayoutInflater();
+        setContentView( R.layout.wait_msg );
+
+        Intent intent = getIntent();
+        String siteNumber = intent.getStringExtra( Airports.SITE_NUMBER );
+
+        AirportRemarksTask task = new AirportRemarksTask();
+        task.execute( siteNumber );
+    }
+
+    private final class AirportRemarksTask extends AsyncTask<String, Void, Cursor[]> {
+
+        @Override
+        protected void onPreExecute() {
+            setProgressBarIndeterminateVisibility( true );
+        }
+
+        @Override
+        protected Cursor[] doInBackground( String... params ) {
+            String siteNumber = params[ 0 ];
+            Cursor[] cursors = new Cursor[ 2 ];
+            
+            DatabaseManager dbManager = DatabaseManager.instance( getApplicationContext() );
+            cursors[ 0 ] = dbManager.getAirportDetails( siteNumber );
+
+            SQLiteDatabase db = dbManager.getDatabase( DatabaseManager.DB_FADDS );
+            SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
+            builder.setTables( Remarks.TABLE_NAME );
+            cursors[ 1 ] = builder.query( db,
+                    new String[] { Remarks.REMARK_NAME, Remarks.REMARK_TEXT },
+                    Runways.SITE_NUMBER+"=? "
+                    +"AND substr("+Remarks.REMARK_NAME+", 1, 2) not in ('A3', 'A4', 'A5', 'A6')",
+                    new String[] { siteNumber }, null, null, Remarks.REMARK_NAME, null );
+
+            return cursors;
+        }
+
+        @Override
+        protected void onPostExecute( Cursor[] result ) {
+            setProgressBarIndeterminateVisibility( false );
+            if ( result == null ) {
+                // TODO: Show an error here
+                return;
+            }
+
+            View view = mInflater.inflate( R.layout.remarks_detail_view, null );
+            setContentView( view );
+            mMainLayout = (LinearLayout) view.findViewById( R.id.airport_remarks_layout );
+
+            // Title
+            Cursor apt = result[ 0 ];
+            GuiUtils.showAirportTitle( mMainLayout, apt );
+
+            showRemarksDetails( result );
+        }
+
+    }
+
+    protected void showRemarksDetails( Cursor[] result ) {
+        Cursor rmk = result[ 1 ];
+        if ( rmk.moveToFirst() ) {
+            LinearLayout layout = (LinearLayout) mMainLayout.findViewById(
+                    R.id.detail_remarks_layout );
+            do {
+                String remark = rmk.getString( rmk.getColumnIndex( Remarks.REMARK_TEXT ) );
+                addRemarkRow( layout, remark );
+            } while ( rmk.moveToNext() );
+        }
+    }
+
+    protected void addRemarkRow( LinearLayout layout, String remark ) {
+        int index = remark.indexOf( ' ' );
+        if ( index != -1 ) {
+            while ( remark.charAt( index ) == ' ' ) {
+                ++index;
+            }
+            remark = remark.substring( index );
+        }
+        LinearLayout innerLayout = new LinearLayout( this );
+        innerLayout.setOrientation( LinearLayout.HORIZONTAL );
+        TextView tv = new TextView( this );
+        tv.setGravity( Gravity.LEFT );
+        tv.setPadding( 12, 1, 2, 1 );
+        tv.setText( "\u2022 " );
+        innerLayout.addView( tv, new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 0f ) );
+        tv = new TextView( this );
+        tv.setGravity( Gravity.LEFT );
+        tv.setPadding( 2, 1, 12, 1 );
+        tv.setText( remark );
+        innerLayout.addView( tv, new LinearLayout.LayoutParams(
+                LayoutParams.WRAP_CONTENT, LayoutParams.WRAP_CONTENT, 1f ) );
+        layout.addView( innerLayout, new LinearLayout.LayoutParams(
+                LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT ) );
+    }
+
+}
