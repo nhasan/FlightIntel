49014f9eabf7197e19e90fcabba321a0066fecb2
Nadeem Hasan <nhasan@nadmm.com>
2011-07-04 01:22:34 -0400

    Calculate earth radius in NM.

--- src/com/nadmm/airports/GeoUtils.java
+++ src/com/nadmm/airports/GeoUtils.java
@@ -27,10 +27,10 @@ public class GeoUtils {
 
     public static final float METERS_PER_STATUTE_MILE = (float) 1609.344;
     public static final float METERS_PER_NAUTICAL_MILE = (float) 1852.0;
-    // Earth's radius at major semi-axis in statute miles
-    private static final double WGS84_a = (6378137.0/METERS_PER_STATUTE_MILE);
-    // Earth's radius at minor semi-axis in statute miles
-    private static final double WGS84_b = (6356752.3/METERS_PER_STATUTE_MILE);
+    // Earth's radius at major semi-axis in nautical miles
+    private static final double WGS84_a = (6378137.0/METERS_PER_NAUTICAL_MILE);
+    // Earth's radius at minor semi-axis in nautical miles
+    private static final double WGS84_b = (6356752.3/METERS_PER_NAUTICAL_MILE);
 
     // Limits in radians
     private static final double MIN_LAT = -Math.PI/2;
@@ -39,7 +39,7 @@ public class GeoUtils {
     private static final double MAX_LON = Math.PI;
 
     public static double getEarthRadius( double radLat ) {
-        // Earth radius at a given lattitude, according to the WGS-84 ellipsoid
+        // Earth radius in nautical miles at a given lattitude, according to the WGS-84 ellipsoid
         // http://en.wikipedia.org/wiki/Earth_radius
         double An = WGS84_a*WGS84_a * Math.cos( radLat );
         double Bn = WGS84_b*WGS84_b * Math.sin( radLat );
