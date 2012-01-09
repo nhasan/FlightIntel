915763f0d255ced55d274ad0587c55144c2e8fa0
Nadeem Hasan <nhasan@nadmm.com>
2012-01-09 17:58:05 -0500

    Added isBetterLocation().

--- src/com/nadmm/airports/utils/GeoUtils.java
+++ src/com/nadmm/airports/utils/GeoUtils.java
@@ -37,6 +37,8 @@ public class GeoUtils {
     private static final double MIN_LON = -Math.PI;
     private static final double MAX_LON = Math.PI;
 
+    private static final int TWO_MINUTES = 1000 * 60 * 2;
+
     public static double getEarthRadius( double radLat ) {
         // Earth radius in nautical miles at a given lattitude, according to the WGS-84 ellipsoid
         // http://en.wikipedia.org/wiki/Earth_radius
@@ -129,6 +131,60 @@ public class GeoUtils {
 
     public static long applyDeclination( long heading, float declination ) {
         return Math.round( heading+declination+360 )%360;
+    }
+
+    /** Determines whether one Location reading is better than the current Location fix
+      * @param location  The new Location that you want to evaluate
+      * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+      */
+    public static boolean isBetterLocation( Location location, Location currentBestLocation ) {
+        if ( currentBestLocation == null ) {
+            // A new location is always better than no location
+            return true;
+        }
+
+        // Check whether the new location fix is newer or older
+        long timeDelta = location.getTime() - currentBestLocation.getTime();
+        boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+        boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+        boolean isNewer = timeDelta > 0;
+
+        // If it's been more than two minutes since the current location, use the new location
+        // because the user has likely moved
+        if ( isSignificantlyNewer ) {
+            return true;
+        // If the new location is more than two minutes older, it must be worse
+        } else if ( isSignificantlyOlder ) {
+            return false;
+        }
+
+        // Check whether the new location fix is more or less accurate
+        int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+        boolean isLessAccurate = accuracyDelta > 0;
+        boolean isMoreAccurate = accuracyDelta < 0;
+        boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+        // Check if the old and new location are from the same provider
+        boolean isFromSameProvider = isSameProvider( location.getProvider(),
+                currentBestLocation.getProvider() );
+
+        // Determine location quality using a combination of timeliness and accuracy
+        if ( isMoreAccurate ) {
+            return true;
+        } else if ( isNewer && !isLessAccurate ) {
+            return true;
+        } else if ( isNewer && !isSignificantlyLessAccurate && isFromSameProvider ) {
+            return true;
+        }
+        return false;
+    }
+
+    /** Checks whether two providers are the same */
+    private static boolean isSameProvider( String provider1, String provider2 ) {
+        if ( provider1 == null ) {
+          return provider2 == null;
+        }
+        return provider1.equals( provider2 );
     }
 
 }
