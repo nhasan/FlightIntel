f84db8167747a63059f8a45568393bceb3a87b68
Nadeem Hasan <nhasan@nadmm.com>
2012-02-19 18:34:51 -0500

    * Make it Serializable *Added ctors

--- src/com/nadmm/airports/wx/Taf.java
+++ src/com/nadmm/airports/wx/Taf.java
@@ -26,29 +26,56 @@ public class Taf implements Serializable  {
 
     private static final long serialVersionUID = 1L;
 
-    public static class TurbulenceCondition {
+    public static class TurbulenceCondition implements Serializable {
+        private static final long serialVersionUID = 1L;
+
         public int intensity;
         public int minAltitudeFeetAGL;
         public int maxAltitudeFeetAGL;
+
+        public TurbulenceCondition() {
+            intensity = Integer.MAX_VALUE;
+            minAltitudeFeetAGL = Integer.MAX_VALUE;
+            maxAltitudeFeetAGL = Integer.MAX_VALUE;
+        }
     }
 
-    public static class IcingCondition {
+    public static class IcingCondition implements Serializable {
+        private static final long serialVersionUID = 1L;
+
         public int intensity;
         public int minAltitudeFeetAGL;
         public int maxAltitudeFeetAGL;
+
+        public IcingCondition() {
+            intensity = Integer.MAX_VALUE;
+            minAltitudeFeetAGL = Integer.MAX_VALUE;
+            maxAltitudeFeetAGL = Integer.MAX_VALUE;
+        }
     }
 
-    public static class Temperature {
+    public static class Temperature implements Serializable {
+        private static final long serialVersionUID = 1L;
+
         public long validTime;
         public float surfaceTempCentigrade;
         public float maxTempCentigrade;
         public float minTempCentigrade;
+
+        public Temperature() {
+            validTime = Long.MAX_VALUE;
+            surfaceTempCentigrade = Float.MAX_VALUE;
+            maxTempCentigrade = Float.MAX_VALUE;
+            minTempCentigrade = Float.MAX_VALUE;
+        }
     }
 
-    public static class Forecast {
-        public long timeFrom;
-        public long timeto;
+    public static class Forecast implements Serializable {
+        private static final long serialVersionUID = 1L;
+
         public String changeIndicator;
+        public long timeFrom;
+        public long timeTo;
         public long timeBecoming;
         public int probability;
         public int windDirDegrees;
@@ -65,11 +92,33 @@ public class Taf implements Serializable  {
         public ArrayList<TurbulenceCondition> turbulenceConditions;
         public ArrayList<IcingCondition> icingConditions;
         public ArrayList<Temperature> temperatures;
+
+        public Forecast() {
+            timeFrom = 0;
+            timeTo = 0;
+            timeBecoming = 0;
+            probability = Integer.MAX_VALUE;
+            windDirDegrees = Integer.MAX_VALUE;
+            windSpeedKnots = Integer.MAX_VALUE;
+            windGustKnots = Integer.MAX_VALUE;
+            windShearDirDegrees = Integer.MAX_VALUE;
+            windShearSpeedKnots = Integer.MAX_VALUE;
+            windShearHeightFeetAGL = Integer.MAX_VALUE;
+            visibilitySM = Float.MAX_VALUE;
+            altimeterHg = Float.MAX_VALUE;
+            vertVisibilityFeet = Integer.MAX_VALUE;
+            wxList = new ArrayList<WxSymbol>();
+            skyConditions = new ArrayList<SkyCondition>();
+            turbulenceConditions = new ArrayList<TurbulenceCondition>();
+            icingConditions = new ArrayList<IcingCondition>();
+            temperatures = new ArrayList<Temperature>();
+        }
     }
 
     public boolean isValid;
     public String stationId;
     public String rawText;
+    public long fetchTime;
     public long issueTime;
     public long bulletinTime;
     public long validTimeFrom;
@@ -79,11 +128,13 @@ public class Taf implements Serializable  {
 
     public Taf() {
         isValid = false;
+        fetchTime = 0;
         issueTime = 0;
         bulletinTime = 0;
         validTimeFrom = 0;
         validTimeTo = 0;
         stationElevationMeters = Float.MAX_VALUE;
+        forecasts = new ArrayList<Forecast>();
     }
 
 }
