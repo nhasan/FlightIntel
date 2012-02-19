008a77555353bcf77e841d0b8b364b1e415b9b69
Nadeem Hasan <nhasan@nadmm.com>
2012-02-19 18:34:12 -0500

    Added new helper functions.

--- src/com/nadmm/airports/utils/TimeUtils.java
+++ src/com/nadmm/airports/utils/TimeUtils.java
@@ -19,11 +19,49 @@
 
 package com.nadmm.airports.utils;
 
+import java.util.Date;
+import java.util.Formatter;
+
+import android.content.Context;
+import android.text.format.DateFormat;
+import android.text.format.DateUtils;
+import android.text.format.Time;
+
 public class TimeUtils {
     
     private static final int SECONDS_PER_MINUTE = 60;
     private static final int SECONDS_PER_HOUR = 60 * 60;
     private static final int SECONDS_PER_DAY = 24 * 60 * 60;
+
+    private static final Formatter sFormatter = new Formatter();
+
+    public static CharSequence formatLongDateTime( long time ) {
+        return DateFormat.format( "MMM dd, yyyy h:mmaa", new Date( time ) );
+    }
+
+    public static CharSequence formatElapsedTime( long time ) {
+        Date now = new Date();
+        return formatElapsedTime( now.getTime(), time );
+    }
+
+    public static CharSequence formatElapsedTime( long time1, long time2 ) {
+        long age = time1-time2;
+        return formatDuration( age )+" old";
+    }
+
+    public static String formatDateRangeUTC( Context context,
+            long startMillis, long endMillis ) {
+        StringBuilder sb = (StringBuilder) sFormatter.out();
+        sb.setLength( 0 );
+        DateUtils.formatDateRange( context, sFormatter, startMillis, endMillis,
+                DateUtils.FORMAT_24HOUR
+                | DateUtils.FORMAT_SHOW_DATE
+                | DateUtils.FORMAT_SHOW_TIME
+                | DateUtils.FORMAT_NO_YEAR
+                | DateUtils.FORMAT_ABBREV_ALL,
+                Time.TIMEZONE_UTC );
+        return sb.toString()+" UTC";
+    }
 
     public static String formatDuration( long duration ) {
         StringBuilder builder = new StringBuilder();
