63fc6c9f0b324180767de7e83fb194e2a8898639
Nadeem Hasan <nhasan@nadmm.com>
2012-03-01 16:34:01 -0500

    DtppActivity moved to its own package.

--- src/com/nadmm/airports/AirportDetailsActivity.java
+++ src/com/nadmm/airports/AirportDetailsActivity.java
@@ -63,6 +63,7 @@ import com.nadmm.airports.DatabaseManager.Tower6;
 import com.nadmm.airports.DatabaseManager.Tower7;
 import com.nadmm.airports.DatabaseManager.Tower8;
 import com.nadmm.airports.DatabaseManager.Wxs;
+import com.nadmm.airports.dtpp.DtppActivity;
 import com.nadmm.airports.utils.CursorAsyncTask;
 import com.nadmm.airports.utils.DataUtils;
 import com.nadmm.airports.utils.GeoUtils;
