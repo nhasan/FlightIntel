e3702676ce2a970da9217541bed3acd275240eec
Nadeem Hasan <nhasan@nadmm.com>
2012-03-16 07:23:48 -0400

    Adde more format helpers.

--- src/com/nadmm/airports/utils/FormatUtils.java
+++ src/com/nadmm/airports/utils/FormatUtils.java
@@ -21,24 +21,91 @@ package com.nadmm.airports.utils;
 
 import java.text.DecimalFormat;
 
+import com.nadmm.airports.wx.WxUtils;
+
 public class FormatUtils {
 
     private static DecimalFormat sFeetFormat;
+    private static DecimalFormat sFeetFormatMsl;
+    private static DecimalFormat sFeetFormatAgl;
     private static DecimalFormat sNumberFormat;
+    private static DecimalFormat sFreqFormat;
+    private static DecimalFormat sVisFormat;
 
     static {
         sFeetFormat = new DecimalFormat();
         sFeetFormat.applyPattern( "#,##0.# ft" );
+        sFeetFormatMsl = new DecimalFormat();
+        sFeetFormatMsl.applyPattern( "#,##0.# ft MSL" );
+        sFeetFormatAgl = new DecimalFormat();
+        sFeetFormatAgl.applyPattern( "#,##0.# ft AGL" );
         sNumberFormat = new DecimalFormat();
         sNumberFormat.applyPattern( "#,##0.##" );
+        sFreqFormat = new DecimalFormat();
+        sFreqFormat.applyPattern( "##0.000" );
+        sVisFormat = new DecimalFormat();
+        sVisFormat.applyPattern( "#0.## SM" );
     }
 
     public static String formatFeet( float value ) {
         return sFeetFormat.format( value );
     }
 
+    public static String formatFeetMsl( float value ) {
+        return sFeetFormatMsl.format( value );
+    }
+
+    public static String formatFeetRangeMsl( int base, int top ) {
+        if ( base < Integer.MAX_VALUE && top < Integer.MAX_VALUE ) {
+            return String.format( "%s to %s ft MSL",
+                    sNumberFormat.format( base ), sNumberFormat.format( top ) );
+        } else if ( base < Integer.MAX_VALUE ) {
+            return String.format( "%s ft MSL and above", sNumberFormat.format( base ) );
+        } else if ( top < Integer.MAX_VALUE ) {
+            return String.format( "%s ft MSL and below", sNumberFormat.format( top ) );
+        } else {
+            return "";
+        }
+    }
+
+    public static String formatFeetAgl( float value ) {
+        return sFeetFormatAgl.format( value );
+    }
+
+    public static String formatFeetRangeAgl( int base, int top ) {
+        if ( base < Integer.MAX_VALUE && top < Integer.MAX_VALUE ) {
+            return String.format( "%s to %s ft AGL",
+                    sNumberFormat.format( base ), sNumberFormat.format( top ) );
+        } else if ( base < Integer.MAX_VALUE ) {
+            return String.format( "%s ft AGL and above", sNumberFormat.format( top ) );
+        } else if ( top < Integer.MAX_VALUE ) {
+            return String.format( "%s ft AGL and below", sNumberFormat.format( top ) );
+        } else {
+            return "";
+        }
+    }
+
     public static String formatNumber( float value ) {
         return sNumberFormat.format( value );
+    }
+
+    public static String formatFreq( float value ) {
+        return sFreqFormat.format( value );
+    }
+
+    public static String formatVisibility( float value ) {
+        return sVisFormat.format( value );
+    }
+
+    public static String formatTemperature( float temp ) {
+        return String.format( "%.1f\u00B0C (%.0f\u00B0F)",
+                temp, WxUtils.celsiusToFahrenheit( temp ) );
+    }
+
+    public static String formatAltimeter( float altimeterHg ) {
+        float altimeterMb = WxUtils.hgToMillibar( altimeterHg );
+        return String.format( "%.2f\" Hg (%s mb)",
+                altimeterHg, FormatUtils.formatNumber( altimeterMb ) );
     }
 
 }
