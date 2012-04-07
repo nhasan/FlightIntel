f7169d2cc953b0ee58d9962132406037ade78cb1
Nadeem Hasan <nhasan@nadmm.com>
2012-04-07 09:58:53 -0400

    New loader for d-AFD metadata.

---
+++ tool/load_dafd_metadata.pl
@@ -0,0 +1,155 @@
+#!/usr/bin/perl
+
+#/*
+# * FlightIntel for Pilots
+# *
+# * Copyright 2012 Nadeem Hasan <nhasan@nadmm.com>
+# *
+# * This program is free software: you can redistribute it and/or modify
+# * it under the terms of the GNU General Public License as published by
+# * the Free Software Foundation, either version 3 of the License, or
+# * (at your option) any later version.
+# *
+# * This program is distributed in the hope that it will be useful,
+# * but WITHOUT ANY WARRANTY; without even the implied warranty of
+# * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# * GNU General Public License for more details.
+# *
+# * You should have received a copy of the GNU General Public License
+# * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+# */
+
+use strict;
+use DBI;
+use LWP::Simple;
+use XML::Twig;
+
+my $BASE_DIR = shift @ARGV;
+# Format: 05APR2012
+our $cycle = shift @ARGV;
+my $AFD_METADATA_FILE = "$BASE_DIR/d-AFD_Metadata.xml";
+my $dafd_url = "http://aeronav.faa.gov/afd/afd_$cycle.xml";
+my $count = 0;
+
+print "Downloading the d-AFD metafile: ".$dafd_url."...";
+my $ret = 200;
+#my $ret = getstore( $dafd_url, $AFD_METADATA_FILE );
+if ( $ret != 200 )
+{
+    die "Unable to download d-AFD metadata.";
+}
+print "done\n";
+
+my $dbfile = "dafd.db";
+my $dbh = DBI->connect( "dbi:SQLite:dbname=$dbfile", "", "" );
+
+$dbh->do( "PRAGMA page_size=4096" );
+$dbh->do( "PRAGMA synchronous=OFF" );
+
+my $create_metadata_table = "CREATE TABLE android_metadata ( locale TEXT );";
+my $insert_metadata_record = "INSERT INTO android_metadata VALUES ( 'en_US' );";
+
+$dbh->do( "DROP TABLE IF EXISTS android_metadata" );
+$dbh->do( $create_metadata_table );
+$dbh->do( $insert_metadata_record );
+
+my $create_cycle_table = "CREATE TABLE cycle ("
+        ."_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        ."AFD_CYCLE TEXT, "
+        ."FROM_DATE TEXT, "
+        ."TO_DATE TEXT"
+        .")";
+
+my $insert_cycle_record = "INSERT INTO cycle ("
+        ."AFD_CYCLE, "
+        ."FROM_DATE, "
+        ."TO_DATE"
+        .") VALUES ("
+        ."?, ?, ?"
+        .")";
+
+my $create_dafd_table = "CREATE TABLE dafd ("
+        ."_id INTEGER PRIMARY KEY AUTOINCREMENT, "
+        ."FAA_CODE TEXT, "
+        ."PDF_NAME TEXT"
+        .")";
+
+my $insert_dafd_record = "INSERT INTO dafd ("
+        ."FAA_CODE, "
+        ."PDF_NAME"
+        .") VALUES ("
+        ."?, ?"
+        .")";
+
+$dbh->do( "DROP TABLE IF EXISTS cycle" );
+$dbh->do( $create_cycle_table );
+my $sth_cycle = $dbh->prepare( $insert_cycle_record );
+
+$dbh->do( "DROP TABLE IF EXISTS dafd" );
+$dbh->do( $create_dafd_table );
+$dbh->do( "CREATE INDEX idx_dafd_faa_code on dafd ( FAA_CODE );" );
+my $sth_dafd = $dbh->prepare( $insert_dafd_record );
+
+my $twig= new XML::Twig(
+                        start_tag_handlers => { 
+                            airports => \&airports,
+                            city_name => \&city_name,
+                            airport_name => \&airport_name },
+                        twig_handlers => {
+                            airport => \&airport } );
+
+$twig->parsefile( $AFD_METADATA_FILE );
+
+print "\rDone loading $count records\n";
+
+exit;
+
+my $cycle;
+my $from_date;
+my $to_date;
+my $volume;
+my $faa_code;
+my $military_use;
+
+sub airports
+{
+    my( $twig, $dafd )= @_;
+    my $from_date = $dafd->{'att'}->{'from_edate'};
+    my $to_date = $dafd->{'att'}->{'to_edate'};
+
+    print $main::cycle."\n";
+    #AFD_CYCLE
+    $sth_cycle->bind_param( 1, $main::cycle );
+    #FROM_DATE
+    $sth_cycle->bind_param( 2, $from_date );
+    #TO_DATE
+    $sth_cycle->bind_param( 3, $to_date );
+
+    $sth_cycle->execute;
+
+    return 1;
+}
+
+sub airport
+{
+    my( $twig, $airport )= @_;
+    my $faa_code = $airport->child_text( 0, "aptid" );
+    my $pdf_name = $airport->child_text( 0, "pdf" );
+
+    if ( length( $faa_code ) > 0 )
+    {
+        #FAA_CODE
+        $sth_dafd->bind_param( 1, $faa_code );
+        #PDF_NAME
+        $sth_dafd->bind_param( 2, $pdf_name );
+    
+        print "\rLoading # $count...";
+    
+        $sth_dafd->execute;
+    
+        $twig->purge;
+        ++$count;
+    }
+
+    return 1;
+}
