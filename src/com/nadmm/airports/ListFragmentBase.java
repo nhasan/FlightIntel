07a583e73e33a18e0f75d4915914511df9205313
Nadeem Hasan <nhasan@nadmm.com>
2012-05-01 18:44:35 -0400

    Refactored base class for custom list fragment.

---
+++ src/com/nadmm/airports/ListFragmentBase.java
@@ -0,0 +1,131 @@
+/*
+ * FlightIntel for Pilots
+ *
+ * Copyright 2011-2012 Nadeem Hasan <nhasan@nadmm.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ */
+
+package com.nadmm.airports;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.os.Bundle;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public abstract class ListFragmentBase extends FragmentBase implements LocationListener {
+
+    private ListView mListView;
+    private String mEmptyText;
+
+    @Override
+    public View onCreateView( LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState ) {
+        View view = inflater.inflate( R.layout.list_view_layout, container, false );
+        mListView = (ListView) view.findViewById( R.id.list_view );
+        mListView.setOnItemClickListener( new OnItemClickListener() {
+
+            @Override
+            public void onItemClick( AdapterView<?> parent, View view,
+                    int position, long id ) {
+                onListItemClick( mListView, view, position );
+            }
+
+        } );
+
+        return createContentView( view );
+    }
+
+    @Override
+    public void onDestroy() {
+        CursorAdapter adapter = (CursorAdapter) mListView.getAdapter();
+        if ( adapter != null ) {
+            Cursor c = adapter.getCursor();
+            c.close();
+        }
+
+        super.onDestroy();
+    }
+
+    protected void setCursor( Cursor c ) {
+        if ( getActivity() == null ) {
+            // We may get called here after activity has detached
+            c.close();
+            return;
+        }
+
+        setListShown( c.getCount() > 0 );
+
+        CursorAdapter adapter = (CursorAdapter) mListView.getAdapter();
+        if ( adapter == null ) {
+            adapter = newListAdapter( getActivity(), c );
+            mListView.setAdapter( adapter );
+        } else {
+            adapter.changeCursor( c );
+        }
+
+        setFragmentContentShown( true );
+    }
+
+    public void setEmptyText( String text ) {
+        mEmptyText = text;
+    }
+
+    public void setListShown( boolean show ) {
+        TextView tv = (TextView) findViewById( R.id.list_msg );
+        if ( show ) {
+            tv.setVisibility( View.GONE );
+            mListView.setVisibility( View.VISIBLE );
+        } else {
+            tv.setText( mEmptyText );
+            tv.setVisibility( View.VISIBLE );
+            mListView.setVisibility( View.GONE );
+        }
+    }
+
+    public ListAdapter getListAdapter() {
+        return mListView.getAdapter();
+    }
+
+    abstract protected CursorAdapter newListAdapter( Context context, Cursor c );
+
+    abstract protected void onListItemClick( ListView l, View v, int position );
+
+    @Override
+    public void onLocationChanged( Location location ) {
+    }
+
+    @Override
+    public void onProviderDisabled( String provider ) {
+    }
+
+    @Override
+    public void onProviderEnabled( String provider ) {
+    }
+
+    @Override
+    public void onStatusChanged( String provider, int status, Bundle extras ) {
+    }
+
+}
