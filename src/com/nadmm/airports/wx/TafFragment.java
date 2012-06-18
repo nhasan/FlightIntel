2f69c43b221bafd9fc813665970a363d30e6cfd6
Nadeem Hasan <nhasan@nadmm.com>
2012-06-18 19:13:05 -0400

    Call the appropriate show() method.

--- src/com/nadmm/airports/wx/TafFragment.java
+++ src/com/nadmm/airports/wx/TafFragment.java
@@ -30,8 +30,8 @@ import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.view.ViewGroup.LayoutParams;
 import android.widget.Button;
 import android.widget.LinearLayout;
@@ -298,7 +298,7 @@ public class TafFragment extends FragmentBase {
             addBulletedRow( layout, "Station has not updated the TAF for more than 12 hours" );
             detail.setVisibility( View.GONE );
             stopRefreshAnimation();
-            setContentShown( true );
+            setFragmentContentShown( true );
             return;
         } else {
             tv.setText( "" );
