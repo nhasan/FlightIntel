f3cba45161b37962e88e4ab335a872bbccf9f49f
Nadeem Hasan <nhasan@nadmm.com>
2012-08-18 18:40:30 -0400

    Added more entries for the first release.

--- tool/load_library.pl
+++ tool/load_library.pl
@@ -77,8 +77,12 @@ $dbh->do( $create_categories_table );
 my $sth_categories = $dbh->prepare( $insert_categories_record );
 
 insert_category( "handbooks", "HANDBOOKS" );
+insert_category( "manuals", "MANUALS" );
 insert_category( "periodicals", "PERIODICALS" );
 insert_category( "regs", "REGULATIONS" );
+insert_category( "risk", "RISK MANAGEMENT" );
+insert_category( "circular", "ADVISORY CIRCULARS" );
+insert_category( "misc", "MISCELLANEOUS" );
 
 $dbh->do( "DROP TABLE IF EXISTS library" );
 $dbh->do( $create_library_table );
@@ -86,39 +90,89 @@ $dbh->do( "CREATE INDEX idx_library_book on library ( CATEGORY_CODE, BOOK_NAME )
 my $sth_library = $dbh->prepare( $insert_library_record );
 
 insert_book( "handbooks", "faa-h-8083-3a-2004.pdf", "Airplane Flying Handbook", 
-        "2004", "FAA", 14042494 );
+        "2004", "FAA", 14042341 );
 insert_book( "handbooks", "faa-h-8083-25a-2008.pdf", "Pilot's Handbook of Aeronautical Knowledge",
-        "2008", "FAA", 56064942 );
-insert_book( "handbooks", "faa-h-8083-27a-2006.pdf", "Student Pilot Guide",
-        "2008", "FAA", 231507 );
+        "2008", "FAA", 56064788 );
 insert_book( "handbooks", "faa-h-8083-15a-2008.pdf", "Instrument Flying Handbook",
-        "2008", "FAA", 36125039 );
+        "2008", "FAA", 36124885 );
 insert_book( "handbooks", "faa-h-8261-1a-2007.pdf", "Instrument Procedures Handbook",
-        "2007", "FAA", 18019925 );
-insert_book( "handbooks", "Chart_Users_Guide_10thEd.pdf", "Aeronautical Chart User's Guide",
-        "10th Edition", "FAA", 17160003 );
+        "2007", "FAA", 18019772 );
+insert_book( "handbooks", "faa-h-8083-6-2009.pdf", "Advanced Avionics Handbook",
+        "2009", "FAA", 20664164 );
+insert_book( "handbooks", "faa-h-8083-27a-2006.pdf", "Student Pilot Guide",
+        "2008", "FAA", 231353 );
+insert_book( "handbooks", "faa-h-8083-9a-2008.pdf", "Aviation Instructor's Handbook",
+        "2008", "FAA", 13672806 );
+insert_book( "handbooks", "faa-h-8083-1a-2007.pdf", "Aircraft Weight and Balance Handbook",
+        "2007", "FAA", 12373020 );
+insert_book( "handbooks", "faa-h-8083-21-2000.pdf", "Rotorcraft Flying Handbook",
+        "2000", "FAA", 16859688 );
+insert_book( "handbooks", "faa-h-8083-2-2009.pdf", "Risk Management Handbook",
+        "2009", "FAA", 11163579 );
+insert_book( "handbooks", "faa-fs-i-8700-1-2003.pdf", "Information for Banner Tow Operations",
+        "2003", "FAA", 845400 );
 
-insert_book( "periodicals", "cb_389.pdf", "Callback", "Issue 389", "NASA", 167376 );
-insert_book( "periodicals", "cb_390.pdf", "Callback", "Issue 390", "NASA", 227682 );
+insert_book( "manuals", "Chart_Users_Guide_10thEd.pdf", "Aeronautical Chart User's Guide",
+        "10th Edition", "FAA", 17159844 );
+insert_book( "manuals", "faa-h-8083-19a-2008.pdf", "Plane Sense - General Aviation Information",
+        "2008", "FAA", 13246143 );
+insert_book( "manuals", "ac-00-6a-1975.pdf", "Aviation Weather",
+        "1975", "FAA", 29916885 );
+insert_book( "manuals", "ac-00-45g-2010.pdf", "Aviation Weather Services",
+        "2010", "FAA/NWS", 12250011 );
+insert_book( "manuals", "00-80T-80.pdf", "Aerodynamics for Naval Aviators",
+        "1965", "H. H. Hurt, Jr.", 23020072 );
+
+insert_book( "periodicals", "cb_389.pdf", "Callback", "Issue 389", "NASA", 167235 );
+insert_book( "periodicals", "cb_390.pdf", "Callback", "Issue 390", "NASA", 227541 );
+insert_book( "periodicals", "cb_391.pdf", "Callback", "Issue 391", "NASA", 211511 );
+insert_book( "periodicals", "12jul-front.pdf", "The Front", "Jul 2012", "NOAA", 2071900 );
+insert_book( "periodicals", "11nov-front.pdf", "The Front", "Nov 2011", "NOAA", 1030336 );
+insert_book( "periodicals", "11jul-front.pdf", "The Front", "Jul 2011", "NOAA", 588846 );
 insert_book( "periodicals", "JulAug2012.pdf", "FAA Safety Briefing",
-        "Jul/Aug 2012", "FAA", 3930855 );
+        "Jul/Aug 2012", "FAA", 3930710 );
 insert_book( "periodicals", "MayJun2012.pdf", "FAA Safety Briefing",
-        "May/Jun 2012", "FAA", 3162708 );
+        "May/Jun 2012", "FAA", 3162563 );
 insert_book( "periodicals", "MarApr2012.pdf", "FAA Safety Briefing",
-        "Mar/Apr 2012", "FAA", 6997082 );
+        "Mar/Apr 2012", "FAA", 6996937 );
 
 insert_book( "regs", "aim_2012_r1.pdf", "Aeronautical Information Manual (AIM)",
-        "2012", "FAA", 8641046 );
+        "2012", "FAA", 8640900 );
 insert_book( "regs", "cfr-2012-title14-vol1.pdf", "Federal Aviation Regulations (FAR)",
-        "Parts 1-59", "US GPO", 9855949 );
+        "Parts 1-59", "US GPO", 9855793 );
 insert_book( "regs", "cfr-2012-title14-vol2.pdf", "Federal Aviation Regulations (FAR)",
-        "Parts 60-109", "US GPO", 15172137 );
+        "Parts 60-109", "US GPO", 15171981 );
 insert_book( "regs", "cfr-2012-title14-vol3.pdf", "Federal Aviation Regulations (FAR)",
-        "Parts 110-199", "US GPO", 4961592 );
+        "Parts 110-199", "US GPO", 4961436 );
 insert_book( "regs", "cfr-2012-title14-vol4.pdf", "Federal Aviation Regulations (FAR)",
-        "Parts 200-1199", "US GPO", 16738523 );
+        "Parts 200-1199", "US GPO", 16738367 );
 insert_book( "regs", "cfr-2012-title14-vol5.pdf", "Federal Aviation Regulations (FAR)",
-        "Parts 1200-End", "US GPO", 2181583 );
+        "Parts 1200-End", "US GPO", 2181427 );
+
+insert_book( "risk", "ga_weather_decision_making.pdf", "General Aviation Pilot’s Guide",
+        "2009", "FAA", 659230 );
+insert_book( "risk", "local_vfr.pdf", "Local VFR Flying", "2012", "FAA", 300310 );
+insert_book( "risk", "night_vfr.pdf", "Night VFR Flying", "2012", "FAA", 294394 );
+insert_book( "risk", "pract_riskman.pdf", "Practical Risk Management", "2012", "FAA", 452295 );
+insert_book( "risk", "vfr_xc.pdf", "VFR XC Flying", "2012", "FAA", 488931 );
+
+insert_book( "circular", "ac-60-22-1991.pdf", "Aeronautical Decision Making",
+        "1991", "FAA", 3915178 );
+insert_book( "circular", "ac-91-13c-1979.pdf", "Cold Weather Operation of Aircraft",
+        "1979", "FAA", 553456 );
+insert_book( "circular", "ac-91-74a-2007.pdf", "Flight In Icing Conditions",
+        "2007", "FAA", 584587 );
+insert_book( "circular", "ac-00-54-1988.pdf", "Pilot Windshear Guide",
+        "1988", "FAA", 4475314 );
+insert_book( "circular", "ac-00-46e-2011.pdf", "Aviation Safety Reporting Program",
+        "2011", "FAA", 46601 );
+
+insert_book( "misc", "aip.pdf", "Aeronautical Information Publication",
+        "21st Edition", "FAA", 14850803 );
+insert_book( "misc", "NAT_IGA_2004.pdf", "North Atlantic Operations Manual",
+        "3rd Edition", "USA", 717842 );
+insert_book( "misc", "pcg.pdf", "Pilot/Controller Glossary",
+        "2012", "FAA", 436621 );
 
 exit;
 
