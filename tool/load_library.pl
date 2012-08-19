1286c2f786aa5317a15f0976d5b3f9ad976315ab
Nadeem Hasan <nhasan@nadmm.com>
2012-08-18 21:49:52 -0400

    Added more books.

--- tool/load_library.pl
+++ tool/load_library.pl
@@ -82,6 +82,7 @@ insert_category( "periodicals", "PERIODICALS" );
 insert_category( "regs", "REGULATIONS" );
 insert_category( "risk", "RISK MANAGEMENT" );
 insert_category( "circular", "ADVISORY CIRCULARS" );
+insert_category( "testguide", "TEST GUIDE" );
 insert_category( "misc", "MISCELLANEOUS" );
 
 $dbh->do( "DROP TABLE IF EXISTS library" );
@@ -90,32 +91,32 @@ $dbh->do( "CREATE INDEX idx_library_book on library ( CATEGORY_CODE, BOOK_NAME )
 my $sth_library = $dbh->prepare( $insert_library_record );
 
 insert_book( "handbooks", "faa-h-8083-3a-2004.pdf", "Airplane Flying Handbook", 
-        "2004", "FAA", 14042341 );
+        "2004", "FAA-H-8083-3A", 14042341 );
 insert_book( "handbooks", "faa-h-8083-25a-2008.pdf", "Pilot's Handbook of Aeronautical Knowledge",
-        "2008", "FAA", 56064788 );
+        "2008", "FAA-H-8083-25A", 56064788 );
 insert_book( "handbooks", "faa-h-8083-15a-2008.pdf", "Instrument Flying Handbook",
-        "2008", "FAA", 36124885 );
+        "2008", "FAA-H-8083-15A", 36124885 );
 insert_book( "handbooks", "faa-h-8261-1a-2007.pdf", "Instrument Procedures Handbook",
-        "2007", "FAA", 18019772 );
+        "2007", "FAA-H-8261-1A", 18019772 );
 insert_book( "handbooks", "faa-h-8083-6-2009.pdf", "Advanced Avionics Handbook",
-        "2009", "FAA", 20664164 );
+        "2009", "FAA-H-8083-6", 20664164 );
 insert_book( "handbooks", "faa-h-8083-27a-2006.pdf", "Student Pilot Guide",
-        "2008", "FAA", 231353 );
+        "2008", "FAA-H-8083-27A", 231353 );
 insert_book( "handbooks", "faa-h-8083-9a-2008.pdf", "Aviation Instructor's Handbook",
-        "2008", "FAA", 13672806 );
+        "2008", "FAA-H-8083-9A", 13672806 );
 insert_book( "handbooks", "faa-h-8083-1a-2007.pdf", "Aircraft Weight and Balance Handbook",
-        "2007", "FAA", 12373020 );
+        "2007", "FAA-H-8083-1A", 12373020 );
 insert_book( "handbooks", "faa-h-8083-21-2000.pdf", "Rotorcraft Flying Handbook",
-        "2000", "FAA", 16859688 );
+        "2000", "FAA-H-8083-21", 16859688 );
 insert_book( "handbooks", "faa-h-8083-2-2009.pdf", "Risk Management Handbook",
-        "2009", "FAA", 11163579 );
+        "2009", "FAA-H-8083-2", 11163579 );
 insert_book( "handbooks", "faa-fs-i-8700-1-2003.pdf", "Information for Banner Tow Operations",
-        "2003", "FAA", 845400 );
+        "2003", "FAA/FS-I-8799-1", 845400 );
 
 insert_book( "manuals", "Chart_Users_Guide_10thEd.pdf", "Aeronautical Chart User's Guide",
         "10th Edition", "FAA", 17159844 );
 insert_book( "manuals", "faa-h-8083-19a-2008.pdf", "Plane Sense - General Aviation Information",
-        "2008", "FAA", 13246143 );
+        "2008", "FAA-H-8083-19A", 13246143 );
 insert_book( "manuals", "ac-00-6a-1975.pdf", "Aviation Weather",
         "1975", "FAA", 29916885 );
 insert_book( "manuals", "ac-00-45g-2010.pdf", "Aviation Weather Services",
@@ -157,15 +158,24 @@ insert_book( "risk", "pract_riskman.pdf", "Practical Risk Management", "2012", "
 insert_book( "risk", "vfr_xc.pdf", "VFR XC Flying", "2012", "FAA", 488931 );
 
 insert_book( "circular", "ac-60-22-1991.pdf", "Aeronautical Decision Making",
-        "1991", "FAA", 3915178 );
+        "1991", "AC-60-22", 3915178 );
 insert_book( "circular", "ac-91-13c-1979.pdf", "Cold Weather Operation of Aircraft",
-        "1979", "FAA", 553456 );
+        "1979", "AC-91-13C", 553456 );
 insert_book( "circular", "ac-91-74a-2007.pdf", "Flight In Icing Conditions",
-        "2007", "FAA", 584587 );
+        "2007", "AC-91-74A", 584587 );
 insert_book( "circular", "ac-00-54-1988.pdf", "Pilot Windshear Guide",
-        "1988", "FAA", 4475314 );
+        "1988", "AC-00-54", 4475314 );
 insert_book( "circular", "ac-00-46e-2011.pdf", "Aviation Safety Reporting Program",
-        "2011", "FAA", 46601 );
+        "2011", "AC-00-46E", 46601 );
+
+insert_book( "testguide", "FAA-G-8082-17F.pdf", "Private Pilot Knowledge Test Guide",
+        "2011", "FAA-G-8082-17F", 559673 );
+insert_book( "testguide", "FAA-G-8082-13F.pdf", "Instrument Rating Knowledge Test Guide",
+        "2012", "FAA-G-8082-13F", 432103 );
+insert_book( "testguide", "FAA-G-8082-5E.pdf", "Commercial Pilot Knowledge Test Guide",
+        "2011", "FAA-G-8082-5E", 708182 );
+insert_book( "testguide", "FAA-G-8082-7F.pdf", "Flight & Ground Instructor Knowledge Test Guide",
+        "2012", "FAA-G-8082-7F", 578458 );
 
 insert_book( "misc", "aip.pdf", "Aeronautical Information Publication",
         "21st Edition", "FAA", 14850803 );
