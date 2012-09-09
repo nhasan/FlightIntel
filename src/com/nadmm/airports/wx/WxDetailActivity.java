82cbb384ea4472118a1ea523f039f327c8946b71
Nadeem Hasan <nhasan@nadmm.com>
2012-09-09 14:08:53 -0400

    * Added list navigation to allow direct navigation to pages * Added a workaround for a race condition that causes menu options to disappear.

--- src/com/nadmm/airports/wx/WxDetailActivity.java
+++ src/com/nadmm/airports/wx/WxDetailActivity.java
@@ -19,46 +19,81 @@
 
 package com.nadmm.airports.wx;
 
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.widget.ArrayAdapter;
 
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.OnNavigationListener;
 import com.nadmm.airports.ActivityBase;
 import com.nadmm.airports.R;
 import com.nadmm.airports.utils.TabsAdapter;
 
-public class WxDetailActivity extends ActivityBase {
+public class WxDetailActivity extends ActivityBase
+                implements OnNavigationListener, ViewPager.OnPageChangeListener {
 
     private TabsAdapter mTabsAdapter;
+    private ViewPager mViewPager;
+    private ActionBar mActionBar;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         setContentView( R.layout.fragment_pager_layout );
 
-        ViewPager pager = (ViewPager) findViewById( R.id.content_pager );
-        mTabsAdapter = new TabsAdapter( this, pager );
         Intent intent = getIntent();
         Bundle args = intent.getExtras();
+
+        mViewPager = (ViewPager) findViewById( R.id.content_pager );
+        mViewPager.setOnPageChangeListener( this );
+
+        mTabsAdapter = new TabsAdapter( this, mViewPager );
         mTabsAdapter.addTab( "METAR", MetarFragment.class, args );
         mTabsAdapter.addTab( "TAF", TafFragment.class, args );
-        mTabsAdapter.addTab( "PIREPs", PirepFragment.class, args );
+        mTabsAdapter.addTab( "PIREP", PirepFragment.class, args );
         mTabsAdapter.addTab( "AIRMET/SIGMET", AirSigmetFragment.class, args );
         mTabsAdapter.addTab( "RADAR", RadarFragment.class, args );
         mTabsAdapter.addTab( "PROGNOSIS CHARTS", ProgChartFragment.class, args );
         mTabsAdapter.addTab( "WINDS/TEMPERATURE", WindFragment.class, args );
+        mTabsAdapter.addTab( "WINDS ALOFT", WindsAloftFragment.class, args );
         mTabsAdapter.addTab( "SIG WX", SigWxFragment.class, args );
         mTabsAdapter.addTab( "CEILING & VISIBILIY", CvaFragment.class, args );
         mTabsAdapter.addTab( "ICING", IcingFragment.class, args );
         mTabsAdapter.addTab( "AREA FORECAST", AreaForecastFragment.class, args );
-        mTabsAdapter.addTab( "WINDS ALOFT", WindsAloftFragment.class, args );
 
+        // Build the data model for the spinner adapter
+        String[] titles = new String[ mTabsAdapter.getCount() ];
+        int pos = 0;
+        while ( pos < mTabsAdapter.getCount() ) {
+            titles[ pos ] = mTabsAdapter.getPageTitle( pos ).toString();
+            ++pos;
+        }
+
+        // Setup list navigation mode
+        mActionBar = getSupportActionBar();
+        mActionBar.setNavigationMode( ActionBar.NAVIGATION_MODE_LIST );
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>( mActionBar.getThemedContext(),
+                R.layout.sherlock_spinner_item, titles );
+        adapter.setDropDownViewResource( R.layout.sherlock_spinner_dropdown_item );
+        mActionBar.setListNavigationCallbacks( adapter, this );
+        mActionBar.setDisplayShowTitleEnabled( false );
+
+        // Setup the pager tabs as an alternate navigation mechanism
         PagerTabStrip tabs = (PagerTabStrip) findViewById( R.id.pager_tabs );
         tabs.setTabIndicatorColor( getResources().getColor( R.color.tab_indicator ) );
 
         if ( savedInstanceState != null ) {
-            pager.setCurrentItem( savedInstanceState.getInt( "wxtab" ) );
+            // Workaround for race conditions in ViewPager
+            // See: http://code.google.com/p/android/issues/detail?id=29472
+            final int lastPos = savedInstanceState.getInt( "wxtab" );
+            mViewPager.post( new Runnable() {
+
+                @Override
+                public void run() {
+                    setCurrentPage( lastPos );
+                }
+            } );
         }
 
         super.onCreate( savedInstanceState );
@@ -69,6 +104,33 @@ public class WxDetailActivity extends ActivityBase {
         super.onSaveInstanceState( outState );
         ViewPager pager = (ViewPager) findViewById( R.id.content_pager );
         outState.putInt( "wxtab", pager.getCurrentItem() );
+    }
+
+    @Override
+    public boolean onNavigationItemSelected( int itemPosition, long itemId ) {
+        mViewPager.setCurrentItem( itemPosition );
+        return false;
+    }
+
+    @Override
+    public void onPageScrollStateChanged( int arg0 ) {
+    }
+
+    @Override
+    public void onPageScrolled( int arg0, float arg1, int arg2 ) {
+    }
+
+    @Override
+    public void onPageSelected( int position ) {
+        mActionBar.setSelectedNavigationItem( position );
+    }
+
+    private void setCurrentPage( int pos ) {
+        // Workaround for race conditions in ViewPager
+        // See: http://code.google.com/p/android/issues/detail?id=29472
+        if ( mViewPager.getCurrentItem() != pos ) {
+            mViewPager.setCurrentItem( pos );
+        }
     }
 
 }
