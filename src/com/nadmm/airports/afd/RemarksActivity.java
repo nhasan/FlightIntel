80673149faa11d16d48061da950be39a3d4e4f80
Nadeem Hasan <nhasan@nadmm.com>
2013-01-24 08:28:20 -0500

    Update copyright year.

--- src/com/nadmm/airports/afd/RemarksActivity.java
+++ src/com/nadmm/airports/afd/RemarksActivity.java
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2011 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2011-2013 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
