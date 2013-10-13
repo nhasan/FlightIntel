f5b160d0295ab0007dc617a071d18d3c3501aa54
Nadeem Hasan <nhasan@nadmm.com>
2013-10-13 10:33:59 -0400

    Cleanup imports.

--- src/com/nadmm/airports/ActivityBase.java
+++ src/com/nadmm/airports/ActivityBase.java
@@ -35,7 +35,6 @@ import android.graphics.Color;
 import android.graphics.drawable.AnimationDrawable;
 import android.graphics.drawable.Drawable;
 import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.app.Fragment;
