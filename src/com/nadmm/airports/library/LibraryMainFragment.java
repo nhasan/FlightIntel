1b456856e0964f80f1af01a08334e16a8e2d62ca
Nadeem Hasan <nhasan@nadmm.com>
2013-11-23 16:25:27 -0500

    Use list navigation in place of tabs.

--- src/com/nadmm/airports/library/LibraryMainFragment.java
+++ src/com/nadmm/airports/library/LibraryMainFragment.java
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nadmm.airports.library;
@@ -23,27 +23,29 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteQueryBuilder;
 import android.os.Bundle;
-import android.support.v4.view.PagerTabStrip;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
-import android.view.Menu;
 import android.view.View;
 import android.view.ViewGroup;
-
 import com.nadmm.airports.DatabaseManager;
 import com.nadmm.airports.DatabaseManager.BookCategories;
 import com.nadmm.airports.FragmentBase;
 import com.nadmm.airports.R;
+import com.nadmm.airports.utils.NavAdapter;
 import com.nadmm.airports.utils.TabsAdapter;
 
-public class LibraryMainFragment extends FragmentBase {
+public class LibraryMainFragment extends FragmentBase
+            implements ActionBar.OnNavigationListener, ViewPager.OnPageChangeListener {
 
     private TabsAdapter mTabsAdapter;
+    private int mFragmentId = -1;
+    private ViewPager mViewPager;
 
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState ) {
-        View view = inflate( R.layout.fragment_pager_layout, container );
+        View view = inflate( R.layout.fragment_pager_no_tab_layout, container );
         return view;
     }
 
@@ -51,11 +53,10 @@ public class LibraryMainFragment extends FragmentBase {
     public void onActivityCreated( Bundle savedInstanceState ) {
         super.onActivityCreated( savedInstanceState );
 
-        ViewPager pager = (ViewPager) findViewById( R.id.content_pager );
-        mTabsAdapter = new TabsAdapter( getActivity(), getChildFragmentManager(), pager );
+        mViewPager = (ViewPager) findViewById( R.id.content_pager );
+        mViewPager.setOnPageChangeListener( this );
 
-        PagerTabStrip tabs = (PagerTabStrip) findViewById( R.id.pager_tabs );
-        tabs.setTabIndicatorColor( getResources().getColor( R.color.tab_indicator ) );
+        mTabsAdapter = new TabsAdapter( getActivity(), getChildFragmentManager(), mViewPager );
 
         SQLiteDatabase db = getDatabase( DatabaseManager.DB_LIBRARY );
         SQLiteQueryBuilder builder = new SQLiteQueryBuilder();
@@ -72,9 +73,43 @@ public class LibraryMainFragment extends FragmentBase {
             } while ( c.moveToNext() );
         }
 
-        if ( savedInstanceState != null ) {
-            pager.setCurrentItem( savedInstanceState.getInt( "libtab" ) );
+        // Build the data model for the spinner adapter
+        String[] titles = new String[ mTabsAdapter.getCount() ];
+        int pos = 0;
+        while ( pos < mTabsAdapter.getCount() ) {
+            titles[ pos ] = mTabsAdapter.getPageTitle( pos ).toString();
+            ++pos;
         }
+
+        // Setup list navigation mode
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode( ActionBar.NAVIGATION_MODE_LIST );
+        NavAdapter adapter = new NavAdapter( actionBar.getThemedContext(), "Library", titles );
+        actionBar.setListNavigationCallbacks( adapter, this );
+        actionBar.setDisplayShowTitleEnabled( false );
+
+        if ( savedInstanceState != null ) {
+            // Workaround for race conditions in ViewPager
+            // See: http://code.google.com/p/android/issues/detail?id=29472
+            final int lastPos = savedInstanceState.getInt( "libtab" );
+            mViewPager.post( new Runnable() {
+
+                @Override
+                public void run() {
+                    setCurrentPage( lastPos );
+                }
+            } );
+        }
+    }
+
+    @Override
+    public void onResume() {
+        ActionBar actionBar = getSupportActionBar();
+        if ( actionBar.getNavigationMode() == ActionBar.NAVIGATION_MODE_LIST ) {
+            actionBar.setSelectedNavigationItem( mFragmentId );
+        }
+
+        super.onResume();
     }
 
     @Override
@@ -82,6 +117,34 @@ public class LibraryMainFragment extends FragmentBase {
         super.onSaveInstanceState( outState );
         ViewPager pager = (ViewPager) findViewById( R.id.content_pager );
         outState.putInt( "libtab", pager.getCurrentItem() );
+    }
+
+    @Override
+    public boolean onNavigationItemSelected( int itemPosition, long itemId ) {
+        setCurrentPage( itemPosition );
+        return true;
+    }
+
+    @Override
+    public void onPageScrollStateChanged( int arg0 ) {
+    }
+
+    @Override
+    public void onPageScrolled( int arg0, float arg1, int arg2 ) {
+    }
+
+    @Override
+    public void onPageSelected( int position ) {
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setSelectedNavigationItem( position );
+    }
+
+    private void setCurrentPage( int pos ) {
+        // Workaround for race conditions in ViewPager
+        // See: http://code.google.com/p/android/issues/detail?id=29472
+        if ( mViewPager.getCurrentItem() != pos ) {
+            mViewPager.setCurrentItem( pos );
+        }
     }
 
 }
