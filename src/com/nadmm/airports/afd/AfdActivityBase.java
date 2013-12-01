f0573285e5b3112d4370435262c301c5c0cd95f5
Nadeem Hasan <nhasan@nadmm.com>
2013-11-30 23:12:26 -0500

    --whitespace.

--- src/com/nadmm/airports/afd/AfdActivityBase.java
+++ src/com/nadmm/airports/afd/AfdActivityBase.java
@@ -14,14 +14,13 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nadmm.airports.afd;
 
 import android.os.Bundle;
 import android.view.View;
-
 import com.nadmm.airports.ActivityBase;
 import com.nadmm.airports.R;
 
