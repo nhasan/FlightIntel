e90b8435cb630658e0e29554bb2148c5166c7b16
Nadeem Hasan <nhasan@nadmm.com>
2013-12-03 22:25:43 -0500

    Show loading status in actionbar.

--- src/com/nadmm/airports/tfr/TfrListFragment.java
+++ src/com/nadmm/airports/tfr/TfrListFragment.java
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nadmm.airports.tfr;
@@ -101,6 +101,7 @@ public class TfrListFragment extends FragmentBase {
         super.onActivityCreated( savedInstanceState );
 
         setActionBarTitle( "TFR List" );
+        setActionBarSubtitle( "Loading..." );
 
         requestTfrList( false );
     }
