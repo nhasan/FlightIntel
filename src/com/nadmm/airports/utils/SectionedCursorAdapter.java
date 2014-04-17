659c59441588aed731fc0a6aa85baa77fce85a23
Nadeem Hasan <nhasan@nadmm.com>
2014-04-17 18:29:43 -0400

    Update license header.

--- src/com/nadmm/airports/utils/SectionedCursorAdapter.java
+++ src/com/nadmm/airports/utils/SectionedCursorAdapter.java
@@ -1,7 +1,8 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2011 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2011-2014 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2012 Google Inc
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nadmm.airports.utils;
