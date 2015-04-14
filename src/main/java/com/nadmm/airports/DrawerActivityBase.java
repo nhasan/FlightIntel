eed4eae030e2dee315485a5af3ab5498db7cd979
Nadeem Hasan <nhasan@nadmm.com>
2015-04-14 06:39:25 -0400

    Small update.

--- src/main/java/com/nadmm/airports/DrawerActivityBase.java
+++ src/main/java/com/nadmm/airports/DrawerActivityBase.java
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2011-2013 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2011-2015 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -182,9 +182,8 @@ public class DrawerActivityBase extends ActivityBase implements ListView.OnItemC
             intent = new Intent( this, E6bActivity.class );
         }
 
-        if ( intent != null ) {
-            mIntent = intent;
-        }
+        mIntent = intent;
+
         if ( mDrawerLayout != null ) {
             mDrawerLayout.closeDrawer( mNavDrawer );
         }
