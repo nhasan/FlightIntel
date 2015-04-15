fb5d2fc24d2bfdba1b5e67e7a4ae78074871bcbc
Nadeem Hasan <nhasan@nadmm.com>
2015-04-14 22:26:49 -0400

    Fixed imports.

--- src/main/java/com/nadmm/airports/ActivityBase.java
+++ src/main/java/com/nadmm/airports/ActivityBase.java
@@ -44,7 +44,6 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
 import android.text.format.Time;
-import android.util.Log;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.Menu;
--- src/main/java/com/nadmm/airports/afd/AfdMainActivity.java
+++ src/main/java/com/nadmm/airports/afd/AfdMainActivity.java
@@ -23,7 +23,6 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.Spinner;
