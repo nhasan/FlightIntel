79d6f11463e83a0d8d6159b2f81a96c65d04e6a4
Nadeem Hasan <nhasan@nadmm.com>
2015-04-18 15:07:09 -0400

    Link checks.

--- src/main/java/com/nadmm/airports/DisclaimerActivity.java
+++ src/main/java/com/nadmm/airports/DisclaimerActivity.java
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nadmm.airports;
@@ -68,7 +68,7 @@ public class DisclaimerActivity extends Activity {
         final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences( this );
         SharedPreferences.Editor editor = prefs.edit();
         editor.putBoolean( PreferencesActivity.KEY_DISCLAIMER_AGREED, agreed );
-        editor.commit();
+        editor.apply();
     }
 
 }
