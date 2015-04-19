5f49db52a8554da5356b2657486c116de825e837
Nadeem Hasan <nhasan@nadmm.com>
2015-04-19 16:09:03 -0400

    Use the new material icon.

--- src/main/java/com/nadmm/airports/aeronav/ChartsDownloadFragment.java
+++ src/main/java/com/nadmm/airports/aeronav/ChartsDownloadFragment.java
@@ -332,7 +332,7 @@ public class ChartsDownloadFragment extends FragmentBase {
         tv = (TextView) findViewById( R.id.charts_download_warning );
         tv.setText( msg );
         tv.setCompoundDrawablesWithIntrinsicBounds(
-                mIsOk? R.drawable.ic_check : R.drawable.delete, 0, 0, 0 );
+                mIsOk? R.drawable.ic_check : R.drawable.ic_highlight_remove, 0, 0, 0 );
         tv.setCompoundDrawablePadding( UiUtils.convertDpToPx( getActivity(), 4 ) );
 
         LinearLayout layout = (LinearLayout) findViewById( R.id.vol_chart_details );
