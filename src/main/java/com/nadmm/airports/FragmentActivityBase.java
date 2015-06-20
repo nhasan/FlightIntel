97a9ecd75b49c42458a1dc4414faa09d0e71417d
Nadeem Hasan <nhasan@nadmm.com>
2015-06-20 17:40:11 -0400

    Use the new layout.

--- src/main/java/com/nadmm/airports/FragmentActivityBase.java
+++ src/main/java/com/nadmm/airports/FragmentActivityBase.java
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2011-2013 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2011-2015 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 package com.nadmm.airports;
@@ -28,7 +28,7 @@ public class FragmentActivityBase extends ActivityBase {
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
 
-        setContentView( R.layout.fragment_activity_layout );
+        setContentView( R.layout.activity_main );
     }
 
     @Override
