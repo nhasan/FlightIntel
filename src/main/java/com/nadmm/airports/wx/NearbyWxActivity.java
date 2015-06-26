08ad59998881128350929f7c5218adf6f172f43e
Nadeem Hasan <nhasan@nadmm.com>
2015-06-26 11:55:05 -0400

    Enable swiperefresh and actionbar autohide.

--- src/main/java/com/nadmm/airports/wx/NearbyWxActivity.java
+++ src/main/java/com/nadmm/airports/wx/NearbyWxActivity.java
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2012 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2012-2015 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,17 +20,62 @@
 package com.nadmm.airports.wx;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
 
-import com.nadmm.airports.FragmentActivityBase;
+import com.nadmm.airports.ActivityBase;
+import com.nadmm.airports.FragmentBase;
+import com.nadmm.airports.ListFragmentBase;
+import com.nadmm.airports.R;
+import com.nadmm.airports.utils.UiUtils;
+import com.nadmm.airports.views.ObservableScrollView;
 
-public class NearbyWxActivity extends FragmentActivityBase {
+public class NearbyWxActivity extends ActivityBase {
+
+    private FragmentBase mCurFragment;
 
     @Override
     protected void onCreate( Bundle savedInstanceState ) {
         super.onCreate( savedInstanceState );
 
+        setContentView( R.layout.activity_main );
+    }
+
+    @Override
+    protected void onPostCreate( Bundle savedInstanceState ) {
+        super.onPostCreate( savedInstanceState );
+
         Bundle args = getIntent().getExtras();
         addFragment( NearbyWxFragment.class, args );
+
+        int actionBarSize = UiUtils.calculateActionBarSize( this );
+        setProgressBarTopWhenActionBarShown( actionBarSize );
+    }
+
+    @Override
+    public void onFragmentStarted( FragmentBase fragment ) {
+        super.onFragmentStarted( fragment );
+
+        ListView listView = ( (ListFragmentBase)fragment ).getListView();
+        registerActionBarAutoHideListView( listView );
+
+        mCurFragment = fragment;
+        enableDisableSwipeRefresh( mCurFragment.isRefreshable() );
+    }
+
+    @Override
+    public boolean canSwipeRefreshChildScrollUp() {
+        return mCurFragment.canSwipeRefreshChildScrollUp();
+    }
+
+    @Override
+    protected void requestDataRefresh() {
+        mCurFragment.requestDataRefresh();
+    }
+
+    @Override
+    protected int getSelfNavDrawerItem() {
+        return NAVDRAWER_ITEM_INVALID;
     }
 
 }
