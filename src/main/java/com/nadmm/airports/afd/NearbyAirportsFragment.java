e3f9e1f313ec0c2356105d12c535b611a1d061ab
Nadeem Hasan <nhasan@nadmm.com>
2016-04-16 18:25:06 -0400

    Adapt to parameter change.

--- src/main/java/com/nadmm/airports/afd/NearbyAirportsFragment.java
+++ src/main/java/com/nadmm/airports/afd/NearbyAirportsFragment.java
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2011-2015 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2011-2016 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -66,7 +66,7 @@ public class NearbyAirportsFragment extends LocationListFragmentBase {
         protected Cursor doInBackground( Void... params ) {
             SQLiteDatabase db = getDatabase( DatabaseManager.DB_FADDS );
 
-            return new NearbyAirportsCursor( db, getLastLocation(), getNearbyRadius() );
+            return new NearbyAirportsCursor( db, getLastLocation(), getNearbyRadius(), null );
         }
 
         @Override
