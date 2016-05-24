c46b802e579907f6268ced31b57c3d7e3081e595
Nadeem Hasan <nhasan@nadmm.com>
2016-05-24 08:30:10 -0400

    Added license header and some refactoring.

--- src/main/java/com/nadmm/airports/views/NestedScrollingListView.java
+++ src/main/java/com/nadmm/airports/views/NestedScrollingListView.java
@@ -1,3 +1,22 @@
+/*
+ * FlightIntel for Pilots
+ *
+ * Copyright 2016 Nadeem Hasan <nhasan@nadmm.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package com.nadmm.airports.views;
 
 import android.content.Context;
@@ -8,74 +27,74 @@ import android.widget.ListView;
 
 public class NestedScrollingListView extends ListView implements NestedScrollingChild {
 
-    private NestedScrollingChildHelper mScrollingChildHelper;
-
-    private void init() {
-        mScrollingChildHelper = new NestedScrollingChildHelper( this );
-        setNestedScrollingEnabled( true );
-    }
+    private NestedScrollingChildHelper mChildHelper;
 
     public NestedScrollingListView( Context context ) {
-        super( context );
-        init();
+        this( context, null );
     }
 
     public NestedScrollingListView( Context context, AttributeSet attrs ) {
-        super( context, attrs );
-        init();
+        this( context, attrs, 0 );
+    }
+
+    public NestedScrollingListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super( context, attrs, defStyleAttr );
+
+        mChildHelper = new NestedScrollingChildHelper( this );
+        setNestedScrollingEnabled( true );
     }
 
     @Override
     public void setNestedScrollingEnabled( boolean enabled ) {
-        mScrollingChildHelper.setNestedScrollingEnabled( enabled );
+        mChildHelper.setNestedScrollingEnabled( enabled );
     }
 
     @Override
     public boolean isNestedScrollingEnabled() {
-        return mScrollingChildHelper.isNestedScrollingEnabled();
+        return mChildHelper.isNestedScrollingEnabled();
     }
 
     @Override
     public boolean startNestedScroll( int axes ) {
-        return mScrollingChildHelper.startNestedScroll( axes );
+        return mChildHelper.startNestedScroll( axes );
     }
 
     @Override
     public void stopNestedScroll() {
-        mScrollingChildHelper.stopNestedScroll();
+        mChildHelper.stopNestedScroll();
     }
 
     @Override
     public boolean hasNestedScrollingParent() {
-        return mScrollingChildHelper.hasNestedScrollingParent();
+        return mChildHelper.hasNestedScrollingParent();
     }
 
     @Override
     public boolean dispatchNestedScroll( int dxConsumed, int dyConsumed, int dxUnconsumed,
                                          int dyUnconsumed, int[] offsetInWindow ) {
-        return mScrollingChildHelper.dispatchNestedScroll( dxConsumed, dyConsumed,
+        return mChildHelper.dispatchNestedScroll( dxConsumed, dyConsumed,
                 dxUnconsumed, dyUnconsumed, offsetInWindow );
     }
 
     @Override
     public boolean dispatchNestedPreScroll( int dx, int dy, int[] consumed, int[] offsetInWindow ) {
-        return mScrollingChildHelper.dispatchNestedPreScroll( dx, dy, consumed, offsetInWindow );
+        return mChildHelper.dispatchNestedPreScroll( dx, dy, consumed, offsetInWindow );
     }
 
     @Override
     public boolean dispatchNestedFling( float velocityX, float velocityY, boolean consumed ) {
-        return mScrollingChildHelper.dispatchNestedFling( velocityX, velocityY, consumed );
+        return mChildHelper.dispatchNestedFling( velocityX, velocityY, consumed );
     }
 
     @Override
     public boolean dispatchNestedPreFling( float velocityX, float velocityY ) {
-        return mScrollingChildHelper.dispatchNestedPreFling( velocityX, velocityY );
+        return mChildHelper.dispatchNestedPreFling( velocityX, velocityY );
     }
 
     @Override
     public void onDetachedFromWindow() {
         super.onDetachedFromWindow();
-        mScrollingChildHelper.onDetachedFromWindow();
+        mChildHelper.onDetachedFromWindow();
     }
 
 }
