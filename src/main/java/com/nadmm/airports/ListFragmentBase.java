ed5a1099ee9ba4cc7b057981995de1086c1abb4d
Nadeem Hasan <nhasan@nadmm.com>
2018-10-20 16:06:25 -0400

    Lint fixes.

--- src/main/java/com/nadmm/airports/ListFragmentBase.java
+++ src/main/java/com/nadmm/airports/ListFragmentBase.java
@@ -28,8 +28,6 @@ import android.support.v4.widget.CursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
