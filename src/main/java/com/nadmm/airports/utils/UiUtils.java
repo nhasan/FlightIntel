c1e062c3947d5f4840ab638c001ea61c90a154a2
Nadeem Hasan <nhasan@nadmm.com>
2019-09-24 20:30:23 -0400

    Removed unused code.

--- src/main/java/com/nadmm/airports/utils/UiUtils.java
+++ src/main/java/com/nadmm/airports/utils/UiUtils.java
@@ -236,27 +236,6 @@ public class UiUtils {
         tv.setCompoundDrawablePadding( UiUtils.convertDpToPx( context, 5 ) );
     }
 
-    /** Calculates the Action Bar height in pixels. */
-    public static int calculateActionBarSize( Context context ) {
-        if ( context == null ) {
-            return 0;
-        }
-
-        Resources.Theme curTheme = context.getTheme();
-        if ( curTheme == null ) {
-            return 0;
-        }
-
-        TypedArray att = curTheme.obtainStyledAttributes( RES_IDS_ACTION_BAR_SIZE );
-        if ( att == null ) {
-            return 0;
-        }
-
-        float size = att.getDimension( 0, 0 );
-        att.recycle();
-        return (int) size;
-    }
-
     public static int getSelectableItemBackgroundResource( Context context ) {
         int[] attrs = new int[]{ R.attr.selectableItemBackground };
         TypedArray typedArray = context.obtainStyledAttributes( attrs );
