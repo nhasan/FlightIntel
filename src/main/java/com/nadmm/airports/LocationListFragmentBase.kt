ff81dd496da3288c1ef46043ca880cd429e061ef
Nadeem Hasan <nhasan@nadmm.com>
2022-01-29 23:10:29 -0500

    Stop using deprecated permission APIs.

--- src/main/java/com/nadmm/airports/LocationListFragmentBase.kt
+++ src/main/java/com/nadmm/airports/LocationListFragmentBase.kt
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2015-2020 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2015-2022 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -20,17 +20,18 @@
 package com.nadmm.airports
 
 import android.Manifest.permission
-import android.app.Activity.RESULT_CANCELED
-import android.app.Activity.RESULT_OK
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.net.Uri
 import android.os.Bundle
+import android.os.Looper
 import android.provider.Settings
 import android.view.View
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
+import com.google.android.gms.common.api.ResolvableApiException
 import com.google.android.gms.location.*
 import com.google.android.material.snackbar.Snackbar
 import com.nadmm.airports.data.DatabaseManager.LocationColumns
@@ -43,11 +44,10 @@ abstract class LocationListFragmentBase : ListFragmentBase() {
     private var mPermissionDenied = false
     protected var lastLocation: Location? = null
         private set
-    private var mFusedLocationClient: FusedLocationProviderClient? = null
-    private var mLocationRequest: LocationRequest? = null
-    private var mLocationCallback: LocationCallback? = null
-    private var mSettingsClient: SettingsClient? = null
-    private var mLocationSettingsRequest: LocationSettingsRequest? = null
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
+    private lateinit var mLocationRequest: LocationRequest
+    private lateinit var mLocationCallback: LocationCallback
+    private lateinit var mRequestPermissionLauncher : ActivityResultLauncher<Array<String>>
 
     protected val nearbyRadius: Int
         get() = activityBase.prefNearbyRadius
@@ -61,16 +61,63 @@ abstract class LocationListFragmentBase : ListFragmentBase() {
         }
 
         isLocationUpdateEnabled = lastLocation == null
+
+        mRequestPermissionLauncher =
+            registerForActivityResult(
+                ActivityResultContracts.RequestMultiplePermissions()
+            ) { permissions ->
+                when {
+                    permissions.getOrDefault(permission.ACCESS_FINE_LOCATION, false) -> {
+                        startLocationUpdates()
+                    }
+                    permissions.getOrDefault(permission.ACCESS_COARSE_LOCATION, false) -> {
+                        startLocationUpdates()
+                    } else -> {
+                        mPermissionDenied = true
+                        setEmptyText("Unable to show nearby facilities.\n"
+                                + "FlightIntel needs location permission.")
+                        setListShown(false)
+                        setFragmentContentShown(true)
+                    }
+                }
+            }
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if (isLocationUpdateEnabled) {
+            setupFusedLocationProvider()
+        } else {
+            // Location was passed to us, so just run the task
+            startLocationTask()
+        }
     }
 
     override fun onResume() {
         super.onResume()
 
         if (isLocationUpdateEnabled) {
-            if (checkPermission()) {
-                activityBase.postRunnable(Runnable { this.startLocationUpdates() }, 0)
-            } else {
-                requestPermission()
+            when {
+                isLocationPermissionGranted() -> {
+                    activityBase.postRunnable( { startLocationUpdates() }, 0)
+                }
+                ActivityCompat.shouldShowRequestPermissionRationale(
+                    requireActivity(),
+                    permission.ACCESS_FINE_LOCATION) -> {
+                    showSnackbar("FlightIntel needs access to this device's location to " +
+                            "show nearby facilities."
+                    ) {
+                        mRequestPermissionLauncher.launch(
+                            arrayOf(
+                                permission.ACCESS_FINE_LOCATION,
+                                permission.ACCESS_COARSE_LOCATION
+                            )
+                        )
+                    }
+                } else -> {
+                    resolvePermission { showApplicationSettings() }
+                }
             }
         }
     }
@@ -83,163 +130,85 @@ abstract class LocationListFragmentBase : ListFragmentBase() {
         }
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-
-        if (isLocationUpdateEnabled) {
-            setupFusedLocationProvider()
-        } else {
-            // Location was passed to us, so just run the task
-            startLocationTask()
-        }
-    }
-
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>,
-                                            grantResults: IntArray) {
-        if (requestCode == PERMISSION_REQUEST_FINE_LOCATION) {
-            when {
-                grantResults.isEmpty() -> {
-                    // If user interaction gets interrupted, permission request is cancelled and
-                    // we get an empty array.
-                }
-                grantResults[0] == PackageManager.PERMISSION_GRANTED -> {
-                    startLocationUpdates()
-                }
-                else -> {
-                    // Set the flag so we do not ask for permission repeatedly during the
-                    // fragment lifetime
-                    mPermissionDenied = true
-
-                    setEmptyText("Unable to show nearby facilities.\n" + "FlightIntel needs location permission.")
-                    setListShown(false)
-                    setFragmentContentShown(true)
-                }
-            }
-        } else {
-            super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        // This is not getting called but it should.
-        if (requestCode == PERMISSION_REQUEST_FINE_LOCATION) {
-            when (resultCode) {
-                RESULT_OK -> {
-                    // Nothing to do. startLocationupdates() gets called in onResume again.
-                }
-                RESULT_CANCELED -> {
-                    isLocationUpdateEnabled = false
-                }
-            }
-        } else {
-            super.onActivityResult(requestCode, resultCode, data)
-        }
-    }
-
-    private fun checkPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(activity!!,
-                permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestPermission() {
-        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(activity!!,
-                permission.ACCESS_FINE_LOCATION)
-        if (shouldProvideRationale) {
-            showSnackbar("FlightIntel needs access to device's location to show nearby facilities.",
-                    View.OnClickListener {
-                        requestPermissions(arrayOf(permission.ACCESS_FINE_LOCATION),
-                                PERMISSION_REQUEST_FINE_LOCATION)
-                    })
-        } else if (!mPermissionDenied) {
-            requestPermissions(arrayOf(permission.ACCESS_FINE_LOCATION),
-                    PERMISSION_REQUEST_FINE_LOCATION)
-        } else {
-            // Set the flag so we do not ask for permission repeatedly during the
-            // fragment lifetime
-            showSnackbar("Please enable location permission in the application details settings.",
-                    View.OnClickListener { showApplicationSettings() })
-        }
+    private fun isLocationPermissionGranted(): Boolean {
+        return (ActivityCompat.checkSelfPermission(
+                requireContext(),
+                permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED)
     }
 
     private fun setupFusedLocationProvider() {
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(activity!!)
-        mSettingsClient = LocationServices.getSettingsClient(activity!!)
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
 
-        makeLocationRequest(activityBase.prefUseGps)
-        makeLocationCallback()
-        buildLocationSettingsRequest()
-    }
+        mLocationRequest = LocationRequest.create().apply {
+            interval = 9000
+            fastestInterval = 5000
+            priority = if (activityBase.prefUseGps)
+                LocationRequest.PRIORITY_HIGH_ACCURACY
+            else
+                LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
+        }
 
-    private fun makeLocationRequest(useGps: Boolean) {
-        mLocationRequest = LocationRequest()
-        mLocationRequest!!.interval = 7000
-        mLocationRequest!!.fastestInterval = 3000
-        mLocationRequest!!.priority = if (useGps)
-            LocationRequest.PRIORITY_HIGH_ACCURACY
-        else
-            LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY
-    }
-
-    private fun makeLocationCallback() {
         mLocationCallback = object : LocationCallback() {
-            override fun onLocationResult(locationResult: LocationResult?) {
-                if (locationResult != null) {
-                    val locations = locationResult.locations
-                    if (locations.size >= 1) {
-                        // Use the latest location
-                        updateLocation(locations[locations.size - 1])
-                    }
-                }
+            override fun onLocationResult(p0: LocationResult) {
+                updateLocation(p0.locations.lastOrNull())
             }
         }
     }
 
-    private fun buildLocationSettingsRequest() {
+    private fun startLocationUpdates() {
+        // First check if location is enabled in the settings
         val builder = LocationSettingsRequest.Builder()
-        builder.addLocationRequest(mLocationRequest!!)
-        mLocationSettingsRequest = builder.build()
+            .addLocationRequest(mLocationRequest)
+        val client = LocationServices.getSettingsClient(requireActivity())
+        client.checkLocationSettings(builder.build())
+            // If location is enabled then register for location updates
+            .addOnSuccessListener(requireActivity()) {
+                try {
+                    mFusedLocationClient.lastLocation
+                        .addOnSuccessListener(requireActivity()) { updateLocation(it) }
+                    mFusedLocationClient.requestLocationUpdates(
+                        mLocationRequest,
+                        mLocationCallback, Looper.getMainLooper()
+                    )
+                    mRequestingLocationUpdates = true
+                } catch (e: SecurityException) {
+                    // Ignore exception
+                }
+            }
+            // If location is not enabled then prompt the user to enable by showing system dialog
+            .addOnFailureListener(requireActivity()) { exception ->
+                if (exception is ResolvableApiException) {
+                    resolvePermission { showLocationSettings() }
+                }
+            }
     }
 
-    private fun startLocationUpdates() {
-        if (activity == null) {
-            return
-        }
-        // First check if location is enabled in the settings
-        mSettingsClient!!.checkLocationSettings(mLocationSettingsRequest)
-                // If location is enabled then register for location updates
-                .addOnSuccessListener(activity!!) { requestLocationUpdates() }
-                // If location is not enabled then prompt the user to enable by showing system dialog
-                .addOnFailureListener(activity!!) {
-                    showSnackbar("Please enable location in settings to see nearby facilities.",
-                            View.OnClickListener { showLocationSettings() })
-
-                    setEmptyText("Unable to show nearby facilities.\n" + "Location is not enabled on this device.")
-                    setListShown(false)
-                    setFragmentContentShown(true)
-                }
+    private fun resolvePermission(func: () -> Unit) {
+        mPermissionDenied = true
+        setEmptyText("Unable to show nearby facilities.\n"
+                + "FlightIntel needs location permission.")
+        setListShown(false)
+        setFragmentContentShown(true)
+        showSnackbar(
+            "Please enable location in Settings to see nearby facilities."
+        ) { func() }
     }
 
     private fun stopLocationUpdates() {
-        mFusedLocationClient!!.removeLocationUpdates(mLocationCallback!!)
-                .addOnCompleteListener(activity!!) { mRequestingLocationUpdates = false }
-    }
-
-    private fun requestLocationUpdates() {
-        if (checkPermission()) {
-            // Get the last location as the starting point
-            mFusedLocationClient!!.lastLocation
-                    .addOnSuccessListener(activity!!) { this.updateLocation(it) }
-
-            mFusedLocationClient!!.requestLocationUpdates(mLocationRequest, mLocationCallback!!, null)
-            mRequestingLocationUpdates = true
-        }
+        mFusedLocationClient.removeLocationUpdates(mLocationCallback)
+            .addOnCompleteListener(requireActivity()) { mRequestingLocationUpdates = false }
     }
 
     private fun updateLocation(location: Location?) {
-        if (location != null) {
-            if (lastLocation == null || location.distanceTo(lastLocation) > 100) {
-                // Preserve battery. Only update the list of we have moved more than 100 meters.
-                lastLocation = location
+        location?.let {
+            if (lastLocation == null || it.distanceTo(lastLocation) > 300) {
+                // Preserve battery. Only update the list of we have moved more than 300 meters.
+                lastLocation = it
                 startLocationTask()
             }
         }
@@ -268,8 +237,4 @@ abstract class LocationListFragmentBase : ListFragmentBase() {
 
     protected abstract fun startLocationTask()
 
-    companion object {
-
-        private const val PERMISSION_REQUEST_FINE_LOCATION = 1
-    }
 }
