9cb4016ff53dacdcc8403aac36f6e1cabcc61738
Nadeem Hasan <nhasan@nadmm.com>
2022-09-18 23:15:37 -0400

    Crash fix.

--- src/main/java/com/nadmm/airports/afd/ServicesFragment.kt
+++ src/main/java/com/nadmm/airports/afd/ServicesFragment.kt
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2011-2021 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2011-2022 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -34,10 +34,12 @@ import com.nadmm.airports.R
 import com.nadmm.airports.data.DatabaseManager.Airports
 import com.nadmm.airports.notams.AirportNotamActivity
 import com.nadmm.airports.utils.DataUtils
+import com.nadmm.airports.utils.UiUtils
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.util.*
+
 
 class ServicesFragment : FragmentBase() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
@@ -62,33 +64,37 @@ class ServicesFragment : FragmentBase() {
 
     private fun showDetails(result: Array<Cursor?>) {
         val apt = result[0] ?: return
-        showAirportTitle(apt)
-        showAirportServices(apt)
-        showFaaServices(apt)
-        showFssServices(apt)
+        try {
+            showAirportTitle(apt)
+            showAirportServices(apt)
+            showFaaServices(apt)
+            showFssServices(apt)
+        } catch (e: Exception) {
+            UiUtils.showToast(requireContext(),"Unexpected error occurred: ${e.message}")
+        }
         setFragmentContentShown(true)
     }
 
     private fun showAirportServices( apt: Cursor ) {
         val services = ArrayList<String>()
-        val other = apt.getString(apt.getColumnIndex(Airports.OTHER_SERVICES))
+        val other = apt.getString(apt.getColumnIndexOrThrow(Airports.OTHER_SERVICES))
         if (other.isNotEmpty()) {
             services.addAll(DataUtils.decodeServices(other))
         }
-        val storage = apt.getString(apt.getColumnIndex(Airports.STORAGE_FACILITY))
+        val storage = apt.getString(apt.getColumnIndexOrThrow(Airports.STORAGE_FACILITY))
         if (storage.isNotEmpty()) {
             services.addAll(DataUtils.decodeStorage(storage))
         }
-        val bottOxygen = apt.getString(apt.getColumnIndex(Airports.BOTTLED_O2_AVAILABLE))
+        val bottOxygen = apt.getString(apt.getColumnIndexOrThrow(Airports.BOTTLED_O2_AVAILABLE))
         if (bottOxygen == "Y") {
             services.add("Bottled Oxygen")
         }
-        val bulkOxygen = apt.getString(apt.getColumnIndex(Airports.BULK_O2_AVAILABLE))
+        val bulkOxygen = apt.getString(apt.getColumnIndexOrThrow(Airports.BULK_O2_AVAILABLE))
         if (bulkOxygen == "Y") {
             services.add("Bulk Oxygen")
         }
+        val chips = findViewById<ChipGroup>(R.id.airport_services_chips)
         if (services.isNotEmpty()) {
-            val chips = findViewById<ChipGroup>(R.id.airport_services_chips)
             for (service in services) {
                 val chip = Chip(activity)
                 chip.text = service
@@ -98,44 +104,43 @@ class ServicesFragment : FragmentBase() {
         } else {
             val tv = findViewById<TextView>(R.id.airport_services_label)
             tv!!.visibility = View.GONE
-            val layout = findViewById<LinearLayout>(R.id.airport_services_chips)
-            layout!!.visibility = View.GONE
+            chips!!.visibility = View.GONE
         }
     }
 
     private fun showFaaServices(apt: Cursor) {
         val layout = findViewById<LinearLayout>(R.id.faa_services_layout)
-        val faaRegion = apt.getString(apt.getColumnIndex(Airports.REGION_CODE))
+        val faaRegion = apt.getString(apt.getColumnIndexOrThrow(Airports.REGION_CODE))
         if (faaRegion.isNotEmpty()) {
             addRow(layout!!, "FAA region", DataUtils.decodeFaaRegion(faaRegion))
         }
-        val artccId = apt.getString(apt.getColumnIndex(Airports.BOUNDARY_ARTCC_ID))
-        val artccName = apt.getString(apt.getColumnIndex(Airports.BOUNDARY_ARTCC_NAME))
+        val artccId = apt.getString(apt.getColumnIndexOrThrow(Airports.BOUNDARY_ARTCC_ID))
+        val artccName = apt.getString(apt.getColumnIndexOrThrow(Airports.BOUNDARY_ARTCC_NAME))
         addRow(layout!!, "ARTCC", "$artccId ($artccName)")
-        val siteNumber = apt.getString(apt.getColumnIndex(Airports.SITE_NUMBER))
-        val notamFacility = apt.getString(apt.getColumnIndex(Airports.NOTAM_FACILITY_ID))
+        val siteNumber = apt.getString(apt.getColumnIndexOrThrow(Airports.SITE_NUMBER))
+        val notamFacility = apt.getString(apt.getColumnIndexOrThrow(Airports.NOTAM_FACILITY_ID))
         val intent = Intent(activity, AirportNotamActivity::class.java)
         intent.putExtra(Airports.SITE_NUMBER, siteNumber)
         addClickableRow(layout, "NOTAM facility", notamFacility, intent)
-        val notamD = apt.getString(apt.getColumnIndex(Airports.NOTAM_D_AVAILABLE))
+        val notamD = apt.getString(apt.getColumnIndexOrThrow(Airports.NOTAM_D_AVAILABLE))
         addRow(layout, "NOTAM D available", if (notamD == "Y") "Yes" else "No")
     }
 
     private fun showFssServices( apt: Cursor ) {
         val layout = findViewById<LinearLayout>(R.id.fss_services_layout)
-        val fssId = apt.getString(apt.getColumnIndex(Airports.FSS_ID))
-        val fssName = apt.getString(apt.getColumnIndex(Airports.FSS_NAME))
+        val fssId = apt.getString(apt.getColumnIndexOrThrow(Airports.FSS_ID))
+        val fssName = apt.getString(apt.getColumnIndexOrThrow(Airports.FSS_NAME))
         addRow(layout!!, "Flight service", "$fssId ($fssName)")
-        var fssPhone = apt.getString(apt.getColumnIndex(Airports.FSS_LOCAL_PHONE))
+        var fssPhone = apt.getString(apt.getColumnIndexOrThrow(Airports.FSS_LOCAL_PHONE))
         if (fssPhone.isEmpty()) {
-            fssPhone = apt.getString(apt.getColumnIndex(Airports.FSS_TOLLFREE_PHONE))
+            fssPhone = apt.getString(apt.getColumnIndexOrThrow(Airports.FSS_TOLLFREE_PHONE))
         }
         addPhoneRow(layout, "FSS phone", fssPhone)
-        val state = apt.getString(apt.getColumnIndex(Airports.ASSOC_STATE))
+        val state = apt.getString(apt.getColumnIndexOrThrow(Airports.ASSOC_STATE))
         if (state != "AK") {
             addPhoneRow(layout, "TIBS", "1-877-4TIBS-WX")
             addPhoneRow(layout, "Clearance delivery", "1-888-766-8287")
-            val faaRegion = apt.getString(apt.getColumnIndex(Airports.REGION_CODE))
+            val faaRegion = apt.getString(apt.getColumnIndexOrThrow(Airports.REGION_CODE))
             if (faaRegion == "AEA") {
                 addPhoneRow(layout, "DC SFRA & FRZ", "1-866-225-7410")
             }
