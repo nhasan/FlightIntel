dc44e162b9e29100d9244629b2fb8808adb726bf
nhasan <nhasan@nadmm.com>
2025-08-03 13:37:21 -0400

    Update work-runtime-ktx to version 2.10.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     implementation ("com.google.android.gms:play-services-location:21.3.0")
     implementation ("com.google.firebase:firebase-messaging-ktx:24.1.2")
     implementation ("androidx.core:core-ktx:1.16.0")
-    implementation ("androidx.work:work-runtime-ktx:2.10.2")
+    implementation ("androidx.work:work-runtime-ktx:2.10.3")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.10.2")
     implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.10.2")
     implementation ("com.google.code.gson:gson:2.13.1")
