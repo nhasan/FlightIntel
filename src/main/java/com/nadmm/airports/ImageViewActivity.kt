e0364daaa25dd9819444c99e29869827250c742f
nhasan <nhasan@nadmm.com>
2025-08-06 21:38:12 -0400

    Refactor ImageViewActivity to Kotlin
    This commit refactors `ImageViewActivity.java` to Kotlin (`ImageViewActivity.kt`), including its nested `ImageViewFragment`.

--- src/main/java/com/nadmm/airports/ImageViewActivity.kt
+++ src/main/java/com/nadmm/airports/ImageViewActivity.kt
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2012-2015 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2012-2025 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,60 +16,58 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.nadmm.airports
 
-package com.nadmm.airports;
+import android.graphics.BitmapFactory
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.ActionBar
+import com.nadmm.airports.utils.UiUtils
+import com.nadmm.airports.views.ImageZoomView
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import androidx.appcompat.app.ActionBar.LayoutParams;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+open class ImageViewActivity : FragmentActivityBase() {
 
-import com.nadmm.airports.utils.UiUtils;
-import com.nadmm.airports.views.ImageZoomView;
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
 
-public class ImageViewActivity extends FragmentActivityBase {
-
-    public static final String IMAGE_TITLE = "IMAGE_TITLE";
-    public static final String IMAGE_SUBTITLE = "IMAGE_SUBTITLE";
-    public static final String IMAGE_PATH = "IMAGE_PATH";
-
-    @Override
-    protected void onCreate( Bundle savedInstanceState ) {
-        super.onCreate( savedInstanceState );
-
-        Bundle args = getIntent().getExtras();
-        String title = args.getString( IMAGE_TITLE );
-        setActionBarTitle( title );
-        String subtitle = args.getString( IMAGE_SUBTITLE );
-        setActionBarSubtitle( subtitle );
-
-        addFragment( ImageViewFragment.class, args );
-    }
-
-    public static class ImageViewFragment extends FragmentBase {
-
-        @Override
-        public View onCreateView( LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState ) {
-            ImageZoomView view = new ImageZoomView( getActivity(), null );
-            view.setId( R.id.main_content );
-            view.setLayoutParams( new ViewGroup.LayoutParams(
-                    LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT ) );
-            Bundle args = getArguments();
-            String path = args.getString( IMAGE_PATH );
-            Bitmap bitmap = BitmapFactory.decodeFile( path );
-            if ( bitmap != null ) {
-                view.setImage( bitmap );
-            } else {
-                UiUtils.showToast( getActivity(), "Unable to show image" );
+        intent.extras?.let { args ->
+            args.getString(IMAGE_TITLE)?.let { title ->
+                setActionBarTitle(title)
             }
-
-            return view;
+            args.getString(IMAGE_SUBTITLE)?.let { subtitle ->
+                setActionBarSubtitle(subtitle)
+            }
+            addFragment(ImageViewFragment::class.java, args)
         }
-
     }
 
+    class ImageViewFragment : FragmentBase() {
+        override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+            val view = ImageZoomView(activity, null)
+            view.setId(R.id.main_content)
+            view.setLayoutParams(
+                ViewGroup.LayoutParams(
+                    ActionBar.LayoutParams.MATCH_PARENT, ActionBar.LayoutParams.MATCH_PARENT
+                )
+            )
+            arguments?.let { args ->
+                val path = args.getString(IMAGE_PATH)
+                val bitmap = BitmapFactory.decodeFile(path)
+                if (bitmap != null) {
+                    view.setImage(bitmap)
+                } else {
+                    UiUtils.showToast(requireActivity(), "Unable to show image")
+                }
+            }
+            return view
+        }
+    }
+
+    companion object {
+        const val IMAGE_TITLE: String = "IMAGE_TITLE"
+        const val IMAGE_SUBTITLE: String = "IMAGE_SUBTITLE"
+        const val IMAGE_PATH: String = "IMAGE_PATH"
+    }
 }
