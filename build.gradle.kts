0bfb0699a962c38245d090e4df038ef44f3fe374
nhasan <nhasan@nadmm.com>
2025-08-31 22:53:52 -0400

    Update app version to 6.5.0
    This commit updates the `versionName` to "6.5.0" and `versionCode` to 65000 in the `build.gradle.kts` file.

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,8 +58,8 @@ android {
         minSdk = 29
         targetSdk = 36
         compileSdk = 36
-        versionName = "6.4"
-        versionCode = 64000
+        versionName = "6.5.0"
+        versionCode = 65000
         buildToolsVersion = "36.0.0"
         vectorDrawables.useSupportLibrary = true
     }
