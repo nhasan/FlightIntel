8c8d0afcd8b87e3fe9be791994ad6b950ff729c5
nhasan <nhasan@nadmm.com>
2025-10-18 19:52:49 -0400

    Refactor IRefreshable to Kotlin
    The `IRefreshable.java` interface has been converted to Kotlin (`IRefreshable.kt`).

--- src/main/java/com/nadmm/airports/IRefreshable.kt
+++ src/main/java/com/nadmm/airports/IRefreshable.kt
@@ -1,7 +1,7 @@
 /*
  * FlightIntel for Pilots
  *
- * Copyright 2015-2018 Nadeem Hasan <nhasan@nadmm.com>
+ * Copyright 2015-2025 Nadeem Hasan <nhasan@nadmm.com>
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,13 +16,12 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+package com.nadmm.airports
 
-package com.nadmm.airports;
-
-public interface IRefreshable {
-
+interface IRefreshable {
     // Is this instance refreshable?
-    boolean isRefreshable();
+    fun isRefreshable(): Boolean
+
     // Perform refresh
-    void requestDataRefresh();
+    fun requestDataRefresh()
 }
